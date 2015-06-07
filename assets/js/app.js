01522dd73f4725db15a20311e97a1b9f40d60348
mamod <mamod.mehyar@gmail.com>
2015-06-08 00:45:25 +0300

    new version

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,36 @@
             //off: 'click'
         });
 
+        $(".fixed-menu-button").click(function(){
+            
+            $('.fixed-overlay').show().animate({
+                opacity : '0.6'
+            }, 250);
+
+            $('.fixed-menu').css({
+                // height : $(window).height() + "px"
+            }).animate({
+                right : 0
+            }, 250);
+        });
+
+        $(".fixed-menu-button-close, .fixed-overlay").click(function(){
+            $('.fixed-overlay').animate({
+                opacity : '0'
+            }, 250, function(){
+                $(this).hide();
+            });
+
+            $('.fixed-menu').css({
+                // height : $(window).height() + "px"
+            }).animate({
+                right : '-250px'
+            }, 250);
+        });
+
+        $('.active a, .disabled a').click(function(){
+        	return false;
+        }); 
         // $('.mainlogo, .flogo').click(function(){
         //     window.location.href = "http://sweefty.com";
         // });
